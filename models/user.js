bd3dacce61ff3154f72e6143cb3a2de3e54933b6
VBulanyi <44462928+VBulanyi@users.noreply.github.com>
2019-11-09 09:40:11 +0300

    authorization updated

--- models/user.js
+++ models/user.js
@@ -42,20 +42,20 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw Promise.reject(new Error('Неправильные почта или пароль'));
       }
-
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw Promise.reject(new Error('Неправильные почта или пароль'));
           }
           return user;
         });
-    });
+    })
+    .catch((error) => error);
 };
 
 module.exports = mongoose.model('user', userSchema);
